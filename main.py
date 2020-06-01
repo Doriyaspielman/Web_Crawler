32f4416546ae65cd3608a317ce1dc601445c13e0
DoriyaSpielman <doriyaspielman@gmail.com>
2020-06-02 00:11:22 +0300

    add request

---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('http://coreyms.com').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('cms_scrape.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary'])
+
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    print(headline)
+
+    summary = article.find('div', class_='entry-content').p.text
+    print(summary)
+
+    print()
+
+    csv_writer.writerow([headline, summary])
+
+csv_file.close()
